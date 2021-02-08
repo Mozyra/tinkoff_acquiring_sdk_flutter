98ef0c654921c8fb60f8cdc8fa723db90c4eb3fc
Dmitry Mozer <newgener@tuta.io>
2021-02-08 14:43:03 +0700

    cleaning

--- ios/Classes/TinkoffAcquiringSdkPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface TinkoffAcquiringSdkPlugin : NSObject<FlutterPlugin>
-@end
--- test/tinkoff_acquiring_sdk_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  const MethodChannel channel = MethodChannel('tinkoff_acquiring_sdk');
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-}
